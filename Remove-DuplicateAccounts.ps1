1f327ca61737de9058b8623cff20ff9dbbaf2462
Jesse Ludeman <jesse.ludeman@gmail.com>
2019-02-28 14:52:30 +1100

    Add files via upload

---
+++ Remove-DuplicateAccounts.ps1
@@ -0,0 +1,65 @@
+$remote_computer = (Get-WmiObject -Class win32_userprofile -ComputerName RFD725 | Select-Object).SID
+$exusers = (Get-ADGroupMember "NOT RF EMPLOYED").SamAccountName
+$array = New-Object System.Collections.ArrayList
+$duplicate_users = New-Object System.Collections.ArrayList
+$users_to_delete = New-Object System.Collections.ArrayList
+
+function Convert-SIDtoUsername
+{
+    # skip the local administrator account... hopefully
+    foreach ($r in ($remote_computer | Select-Object -skip 1))
+    {
+        $objSID = New-Object System.Security.Principal.SecurityIdentifier($r)
+        $objUser = $objSID.Translate([System.Security.Principal.NTAccount])
+        [void]$objUser.Value
+        
+        $string_result = $objUser.ToString()
+        $result = $string_result.Replace("RFHV\", "")
+            
+        [void]$array.Add($result)
+    }
+}
+
+function Update-Array
+{
+    $array.Remove("pdq.sa")
+    $array.Remove("Administrator.RFHV")
+    $array.Remove("default")
+    $array.Remove("administrator")
+    $array.Remove("RFHV\Administrator")
+    $array.Remove("NT AUTHORITY\NETWORK SERVICE")
+    $array.Remove("NT AUTHORITY\LOCAL SERVICE")
+    $array.Remove("NT AUTHORITY\SYSTEM")
+}
+
+function Find-DuplicateUser
+{
+    foreach ($a in $array)
+    {
+        foreach ($exuser in $exusers)
+        {
+            if ($a -eq $exuser)
+            {
+                [void]$duplicate_users.add($a)
+            }
+        }
+    }
+}
+
+function Remove-DuplicateUsers
+{
+    foreach ($user in $duplicate_users)
+    {
+        [void]$users_to_delete.Add("C:\users\$user")
+    }
+
+    # Get-WmiObject -Class win32_userprofile -ComputerName RFD725 | Where-Object {$_.localpath -in $users_to_delete} | Remove-WmiObject
+
+    # Write-Output "Operation complete"
+}
+
+# "Main" method here
+Convert-SIDtoUsername
+Update-Array
+Find-DuplicateUser
+Remove-DuplicateUsers
